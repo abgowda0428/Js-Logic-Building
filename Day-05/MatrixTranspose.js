6591a6888f167def1dc010c3eb8c1c89abe62349
AB_GOWDA <abhishekgowda8090@gmail.com>
2025-02-01 15:32:39 +0530

    Completed with the Last Problem of Day-05 in Logic Building in JavaScript

--- Day-05/MatrixTranspose.js
+++ Day-05/MatrixTranspose.js
@@ -1,1 +1,18 @@
-// Solve Matrix Transpose Problem.
+// Solve Matrix Transpose Problem. 
+
+function Matrix(Value){
+    let res = [];
+    for(let i = 0;i<= Value.length-1;i++){
+        for(let j = 0;j<=Value[0].length-1;j++){
+            if(!res[j]){
+                res [j] = []
+            }
+            res [j][i] = Value[i][j];
+        }
+    }
+    console.log(res)
+};
+
+let Martixarr = [[3,4,8],[5,6,9]]
+
+Matrix(Martixarr)
--- Day-05/pyramid.js
+++ Day-05/pyramid.js
@@ -1,12 +1,13 @@
 // Printing the Number Pyramind.
 
 function Pyramid(n){
+  let spaces = "";
     for(let j =1;j<=n;j++){
-      let spaces = "";
+      
       for(let i =1;i<=n;i++){
        spaces = spaces + "";
       };
-      console.log(spaces,j)
+      
     };
 };
 
